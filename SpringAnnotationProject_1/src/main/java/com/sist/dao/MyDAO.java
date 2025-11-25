bcd3f7883012764c5a44b7ccc2958cb947e936f9
dasolson <you@example.com>
2025-11-25 13:55:41 +0900

    1111

---
+++ SpringAnnotationProject_1/src/main/java/com/sist/dao/MyDAO.java
@@ -0,0 +1,54 @@
+package com.sist.dao;
+
+import org.springframework.stereotype.Repository;
+
+/*
+ *  	1. AOP
+ *          = Before
+ *          = After
+ *          = After-Throwing
+ *          = After
+ *          = Around
+ *          
+ *          public String display() {
+ *          	String res = ""
+ *          	=> Before
+ *          	try {
+ *              	----------------- setAutoCommit(false) Around
+ *              	코딩
+ *                  ----------------- commit()	
+ *              }catch {
+ *              	=> After-Throwing
+ *              }finally {
+ *              	=> After
+ *              }
+ *              return res; => After-Returning
+ *          }
+ */
+@Repository("dao")
+public class MyDAO {
+	/*
+	 * public void getConnection() { System.out.println("🔔🔔🔔오라클 연결🔔🔔🔔"); }
+	 * public void disConnection() { System.out.println("🔒🔒🔒오라클 연결 해제🔒🔒🔒"); }
+	 */
+	public void select() {
+		//getConnection(); // bofore
+		System.out.println("데이터 읽기");
+		//disConnection(); // after
+	}
+	public void insert() {
+		//getConnection();
+		System.out.println("데이터 추가");
+		//disConnection();
+	}
+	public void update() {
+		//getConnection();
+		System.out.println("데이터 수정");
+		//disConnection();
+	}
+	public void delete() {
+		//getConnection();
+		System.out.println("데이터 삭제");
+		//disConnection();
+	}
+}
