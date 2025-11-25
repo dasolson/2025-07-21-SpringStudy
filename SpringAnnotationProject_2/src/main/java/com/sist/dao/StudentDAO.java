73f7cf06671a944b99a652492c7ac62c7ddf935b
dasolson <you@example.com>
2025-11-25 16:07:45 +0900

    1111

---
+++ SpringAnnotationProject_2/src/main/java/com/sist/dao/StudentDAO.java
@@ -0,0 +1,50 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sist.vo.*;
+import com.sist.mapper.*;
+// Spring => 메모리 할당 안한다 : Mapper / VO
+@Repository
+public class StudentDAO {
+	@Autowired // getBean
+	private StudentMapper mapper; // 구현된 클래스 대입 요청
+	
+	/*
+	 * @Insert("INSERT INTO student VALUES(#{hakbun}, #{name}, #{kor}, #{eng}, #{math})")
+		public void studentInsert(StudentVO vo);
+		// 2. select
+		@Select("SELECT * FROM student ORDER BY hakbun")
+		public List<StudentVO> studentListData();
+		// 3. update
+		@Update("UPDATE student SET kor=#{kor}, eng=#{eng}, math=#{math} WHERE hakbun=#{hakbun}")
+		public void studentUpdate(StudentVO vo);
+		// 4. select
+		@Select("SELECT * FROM student WHERE hakbun=#{hakbun}")
+		public StudentVO studentDetailData(int hakbun);
+		// 5. delete
+		@Delete("DELETE FROM student WHERE hakbun=#{hakbun}")
+		public void studentDelete(int hakbun);
+	 */
+	public void studentInsert(StudentVO vo) {
+		mapper.studentInsert(vo);
+	}
+	public List<StudentVO> studentListData() {
+		return mapper.studentListData();
+	}
+	public void studentUpdate(StudentVO vo) {
+		mapper.studentUpdate(vo);
+	}
+	public StudentVO studentDetailData(int hakbun) {
+		return mapper.studentDetailData(hakbun);
+	}
+	public void studentDelete(int hakbun) {
+		mapper.studentDelete(hakbun);
+	}
+}
