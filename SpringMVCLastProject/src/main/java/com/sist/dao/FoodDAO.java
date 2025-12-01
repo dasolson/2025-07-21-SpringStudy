8424d71f8934e40a15cc5b49e630de40550d448d
dasolson <you@example.com>
2025-12-01 17:51:03 +0900

    2025-12-01

---
+++ SpringMVCLastProject/src/main/java/com/sist/dao/FoodDAO.java
@@ -0,0 +1,76 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sist.mapper.*;
+import com.sist.vo.*;
+@Repository
+public class FoodDAO {
+	// 구현된 Mapper클래스 읽기
+	@Autowired
+	private FoodMapper mapper;
+	
+	/*
+	 * @Select("SELECT fno, name, poster, num "
+				+ "FROM (SELECT fno, name, poster, rownum as num "
+				+ "FROM (SELECT fno, name, poster "
+				+ "FROM menupan_food ORDER BY fno ASC)) "
+				+ "WHERE num BETWEEN #{start} AND #{end}")
+		public List<FoodVO> foodListData(@Param("start") int start, @Param("end") int end);
+		
+		@Select("SELECT CEIL(COUNT(*)/12.0) FROM menupan_food")
+		public int foodTotalPage();
+	 */
+	public List<FoodVO> foodListData(int start, int end){
+		return mapper.foodListData(start, end);
+	}
+	
+	public int foodTotalPage() {
+		return mapper.foodTotalPage();
+	}
+	
+	/*
+	 * @Update("UPDATE menupan_food SET "
+				+ "hit=hit+1 "
+				+ "WHERE fno=#{fno}")
+		public void foodhitIncrement(int fno);
+		
+		@Select("SELECT fno, name, poster, image, address, phone, parking, time, type, score, theme, content, price "
+				+ "FROM menupan_food "
+				+ "WHERE fno=#{fno}")
+		public FoodVO foodDetailData(int fno);
+	 */
+	public FoodVO foodDetailData(int fno) {
+		mapper.foodhitIncrement(fno);
+		return mapper.foodDetailData(fno);
+	}
+	public FoodVO foodCookieData(int fno) {
+		return mapper.foodDetailData(fno);
+	}
+	
+	/*
+	 * @Select("SELECT fno, name, poster, num "
+				+ "FROM (SELECT fno, name, poster, rownum as num "
+				+ "FROM (SELECT fno, name, poster "
+				+ "FROM menupan_food "
+				+ "WHERE REGEXP_LIKE(address, #{address}) "
+				+ "ORDER BY fno ASC)) "
+				+ "WHERE num BETWEEN #{start} AND #{end}")
+		public List<FoodVO> foodFindData(Map map);
+		
+		@Select("SELECT CEIL(COUNT(*)/12.0) FROM menupan_food "
+				+ "WHERE REGEXP_LIKE(address, #{address})")
+		public int foodFindTotalPage(String address);
+	 */
+	public List<FoodVO> foodFindData(Map map){
+		return mapper.foodFindData(map);
+	}
+	public int foodFindTotalPage(String address) {
+		return mapper.foodFindTotalPage(address);
+	}
+}
---
+++ SpringMVCLastProject/src/main/java/com/sist/service/FoodService.java
@@ -0,0 +1,16 @@
+package com.sist.service;
+import java.util.*;
+/*
+ * 		클라이언트 ======= Service ======= DAO ======= Oracle
+ *                                       |
+ *                                      수정
+ */
+import com.sist.vo.FoodVO;
+public interface FoodService {
+	public List<FoodVO> foodListData(int start, int end);
+	public int foodTotalPage();
+	public FoodVO foodDetailData(int fno);
+	public FoodVO foodCookieData(int fno);
+	public List<FoodVO> foodFindData(Map map);
+	public int foodFindTotalPage(String address);
+}
